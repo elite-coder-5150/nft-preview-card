c81893dc8e35fabc196bc1b3c744fb7820bfa31a
elite-coder-5150 <parkhousedarrell7@gmail.com>
2022-12-10 21:13:04 -0600

    added the app and modal classes

--- js/app.js
+++ js/app.js
@@ -1,1 +1,39 @@
-console.log('working');
+class App {
+    constructor() {
+        this.message = 'Hello World!';
+    }
+
+    init() {
+        this.cacheDom();
+        this.initEvents();
+    }
+
+    cacheDom() {
+        this.$message = document.querySelector('.message');
+    }
+
+    initEvents() {
+        this.$message.addEventListener('click', this.showMessage.bind(this));
+    }
+}
+
+let app = new App();
+app.init();
+
+// create me a modal class
+class Modal {
+    constructor() {
+        this.message = 'Hello World!';
+    }
+
+    cacheDom() {
+        this.modal = document.querySelector('.modal');
+    }
+    open() {
+        this.modal.classList.add('open');
+    }
+
+    close() {
+        this.modal.classList.remove('open');
+    }
+}
